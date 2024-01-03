a1b69b3ca41a42726bf529ec31153b8aa80aaa13
CodeBernardo <stein.bernardo@proton.me>
2024-01-03 10:34:48 -0300

    feat: workout register form

--- src/components/forms/workout_register_form/index.tsx
+++ src/components/forms/workout_register_form/index.tsx
@@ -1,15 +1,40 @@
 import { useForm } from "react-hook-form";
+import { Input, Select } from "../inputs";
+import { days, reps } from "./workout_options";
 
 const WorkoutRegisterForm = () => {
   const { register, handleSubmit } = useForm();
 
   const submit = (payload: unknown) => {
-    console.log(payload)
-  }
+    console.log(payload);
+  };
 
   return (
-    <form action="">
-      
+    <form action="" onSubmit={handleSubmit(submit)}>
+      <Input
+        label={"Exercício"}
+        id={"workout"}
+        type={"text"}
+        {...register("workout")}
+      />
+      <Select
+        label={"Dia da semana"}
+        id={"day"}
+        options={days}
+        {...register("day")}
+      ></Select>
+      <Select
+        label={"Objetivo"}
+        id={"goal"}
+        options={reps}
+        {...register("goal")}
+      ></Select>
+      <div>
+        <button>Cadastrar série</button>
+        <div role="button">Cancelar</div>
+      </div>
     </form>
-  )
+  );
 };
+
+export { WorkoutRegisterForm };
---
+++ src/components/forms/workout_register_form/workout_options/index.tsx
@@ -0,0 +1,48 @@
+import { SelectOpt } from "../../inputs";
+
+// const goals: SelectOpt[] = [
+//   {
+//     value:
+//       "Levantamento de cargas mais pesadas, com períodos de descanso mais longos entre as séries.",
+//     label: "Força",
+//   },
+//   {
+//     value:
+//       "Levantamento de cargas moderadas a pesadas para promover o aumento do tamanho muscular.",
+//     label: "Hipertrofia",
+//   },
+//   {
+//     value:
+//       "Levantamento de cargas mais leves com maior número de repetições para melhorar a resistência muscular.",
+//     label: "Resistência",
+//   },
+//   {
+//     value:
+//       "Pode envolver movimentos rápidos e dinâmicos com menor resistência para melhorar a resistência cardiovascular.",
+//     label: "Cardio",
+//   },
+//   {
+//     value: "Movimentos explosivos para desenvolver a potência muscular.",
+//     label: "Potência",
+//   },
+// ];
+
+const reps: SelectOpt[] = [
+  { value: "4 x 1-6", label: "Força" },
+  { value: "4 x 8-12", label: "Hipertrofia" },
+  { value: "3 x 15+", label: "Resisência Muscular" },
+  { value: "3 x 12-20", label: "Cardio" },
+  { value: "3 x 1-5", label: "Potência Muscular" },
+];
+
+const days: SelectOpt[] = [
+  { value: "Sunday", label: "Domingo" },
+  { value: "Monday", label: "Segunda-feira" },
+  { value: "Tuesday", label: "Terça-feira" },
+  { value: "Wednesday", label: "Quarta-feira" },
+  { value: "Thursday", label: "Quinta-feira" },
+  { value: "Friday", label: "Sexta-feira" },
+  { value: "Saturday", label: "Sábado" },
+];
+
+export { reps, days };
